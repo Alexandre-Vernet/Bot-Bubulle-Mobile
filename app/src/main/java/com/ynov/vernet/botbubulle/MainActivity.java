53ba238ddf8c6f31ba0cc84eab3b437794990719
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2020-10-07 19:49:02 +0200

    Création du compte à rebours avant 22h sur l'activité principale

--- app/src/main/java/com/ynov/vernet/botbubulle/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/botbubulle/MainActivity.java
@@ -8,16 +8,59 @@ import android.hardware.biometrics.BiometricPrompt;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.CancellationSignal;
+import android.os.Handler;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int tHeures, tMinutes, tSecondes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Référencer les interface
+        final TextView heures = (TextView) findViewById(R.id.heures);
+        final TextView minutes = (TextView) findViewById(R.id.minutes);
+        final TextView secondes = (TextView) findViewById(R.id.secondes);
+
+        // Démarrer un thread
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        // Actualiser toutes les secondes le thread
+                        Thread.sleep(1000);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // Récupérer le temps avant 22h
+                                Date date = Calendar.getInstance().getTime();
+                                tHeures = 21 - date.getHours();
+                                tMinutes = 59 - date.getMinutes();
+                                tSecondes = 59 - date.getSeconds();
+
+                                // Afficher le temps
+                                heures.setText("" + tHeures + " h ");
+                                minutes.setText("" + tMinutes + " mn ");
+                                secondes.setText("" + tSecondes + " s");
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+
+        thread.start();
+
     }
 }
