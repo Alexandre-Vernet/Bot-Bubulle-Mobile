26234df4530266f48c8e4b686feb65550aa86556
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2020-10-20 20:27:40 +0200

    Ouverture du clavier automatique classe Code

--- app/src/main/java/com/ynov/vernet/botbubulle/Code.java
+++ app/src/main/java/com/ynov/vernet/botbubulle/Code.java
@@ -3,7 +3,7 @@ package com.ynov.vernet.botbubulle;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
@@ -14,7 +14,6 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Code extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +27,32 @@ public class Code extends AppCompatActivity {
         // Ouvrir le clavier automatiquement
         editTextCode.requestFocus();
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
-
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+
+        // Clic sur le bouton si on appui sur la touche entrée du clavier
+        editTextCode.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Si le code est bon
+                    if (editTextCode.getText().toString().equals("2711")) {
+                        // Démarrer l'activité principale
+                        Toast.makeText(Code.this, "Code bon", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                        finish();
+
+                        // Si le code est mauvais
+                    } else {
+                        editTextCode.setText("");
+                        Toast.makeText(Code.this, "Code incorrect", Toast.LENGTH_SHORT).show();
+                    }
+                    return true;
+                }
+                return false;
+            }
+
+        });
 
         // Au clic du bouton
         btnValider.setOnClickListener(new View.OnClickListener() {
