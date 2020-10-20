5a7b9a01da82bacce601e605bdcaa347c4409019
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2020-10-20 21:19:16 +0200

    Vibration du téléphone lorque le mot de passe est incorrect

--- app/src/main/java/com/ynov/vernet/botbubulle/Code.java
+++ app/src/main/java/com/ynov/vernet/botbubulle/Code.java
@@ -3,6 +3,7 @@ package com.ynov.vernet.botbubulle;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
@@ -15,16 +16,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Code extends AppCompatActivity {
 
+    EditText editTextCode;
+    Button btnValider;
+    Vibrator vibe;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_code);
 
-        // Référencement
-        final EditText editTextCode = findViewById(R.id.editTextCode);
-        Button btnValider = findViewById(R.id.btnValider);
+        // Référencement des éléments graphiques
+        editTextCode = findViewById(R.id.editTextCode);
+        btnValider = findViewById(R.id.btnValider);
 
-        // Ouvrir le clavier automatiquement
+        // Ouvrir le clavier à l'ouverture de l'activité
         editTextCode.requestFocus();
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -34,44 +39,49 @@ public class Code extends AppCompatActivity {
         editTextCode.setOnKeyListener(new View.OnKeyListener() {
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
-                    // Si le code est bon
-                    if (editTextCode.getText().toString().equals("2711")) {
-                        // Démarrer l'activité principale
-                        Toast.makeText(Code.this, "Code bon", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                        startActivity(intent);
-                        finish();
-
-                        // Si le code est mauvais
-                    } else {
-                        editTextCode.setText("");
-                        Toast.makeText(Code.this, "Code incorrect", Toast.LENGTH_SHORT).show();
-                    }
+                    clickBtnValider();
                     return true;
+                } else {
+                    return false;
                 }
-                return false;
             }
-
         });
 
         // Au clic du bouton
         btnValider.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Vérifier si le code est bon
-                if (editTextCode.getText().toString().equals("2711")) {
-                    // Démarrer l'activité principale
-                    Toast.makeText(Code.this, "Code bon", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                    // Si le code est mauvais
-                } else {
-                    editTextCode.setText("");
-                    Toast.makeText(Code.this, "Code incorrect", Toast.LENGTH_SHORT).show();
-                }
+                clickBtnValider();
             }
         });
+    }
+
+    public void clickBtnValider() {
+        // Si le code est bon
+        if (editTextCode.getText().toString().equals("2711")) {
+            // Démarrer l'activité principale
+            Toast.makeText(Code.this, "Code bon", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        // Si le code est mauvais
+        else {
+            // Vider la zone de texte
+            editTextCode.setText("");
+            Toast.makeText(Code.this, "Code incorrect", Toast.LENGTH_SHORT).show();
+
+            // Vibrer
+            vibe = (Vibrator) Code.this.getSystemService(Context.VIBRATOR_SERVICE);
+            vibe.vibrate(400);
+
+            // Ré-ouvrir le clavier
+            editTextCode.requestFocus();
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+        }
     }
 
     // Bouton retour
