126a9b46463d21a6432f8fc4ab8cb7a6f0d3b55a
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2020-12-08 09:59:43 +0100

    Envoyer la notification dans un créneau horaire définit

--- app/src/main/java/com/ynov/vernet/botbubulle/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/botbubulle/MainActivity.java
@@ -1,21 +1,33 @@
 package com.ynov.vernet.botbubulle;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private int tHeures, tMinutes, tSecondes;
-
+    Context context;
     ProgressBar progressBar;
+    private static final String CHANNEL_ID = "Mon canal";
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +39,22 @@ public class MainActivity extends AppCompatActivity {
         final TextView minutes = findViewById(R.id.minutes);
         final TextView secondes = findViewById(R.id.secondes);
         progressBar = findViewById(R.id.progressBar);
+
+
+        // Vérifier l'heure pour l'envoie de la notif
+        Calendar now = Calendar.getInstance();
+
+        int hour = now.get(Calendar.HOUR_OF_DAY); // Get hour in 24 hour format
+        int minute = now.get(Calendar.MINUTE);
+
+        Date date = parseDate(hour + ":" + minute);
+        Date dateCompareOne = parseDate("09:56");
+        Date dateCompareTwo = parseDate("11:00");
+
+        if (dateCompareOne.before(date) && dateCompareTwo.after(date)) {
+            Log.d(TAG, "run: Dring Dring !");
+            envoyerNotification();
+        }
 
 
         // Démarrer un thread
@@ -64,6 +92,62 @@ public class MainActivity extends AppCompatActivity {
         };
 
         thread.start();
+
+    }
+
+    private Date parseDate(String date) {
+
+        final String inputFormat = "HH:mm";
+        SimpleDateFormat inputParser = new SimpleDateFormat(inputFormat, Locale.FRANCE);
+        try {
+            return inputParser.parse(date);
+        } catch (java.text.ParseException e) {
+            return new Date(0);
+        }
+    }
+
+    private void envoyerNotification() {
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+        final PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(
+                        MainActivity.this,
+                        0,
+                        intent,
+                        PendingIntent.FLAG_CANCEL_CURRENT
+                );
+//
+//
+//        // Créer la notification
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentText("Dring Dring ⏲ !")
+                .setSmallIcon(R.drawable.icon)
+                .setLargeIcon(BitmapFactory.decodeResource(this.getResources(), R.drawable.icon))
+
+                .setColor(getResources().getColor(R.color.colorPrimary))
+//                .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+
+//                .setSound(Uri.parse("https://www.youtube.com/watch?v=W6YOhFCGZMM&list=WL&index=2"))
+
+
+                .setSound(Uri.parse("android.resource://com.ynov.vernet.botbubulle/" + R.raw.bulles))
+
+                .setDefaults(-1)
+
+
+                .setStyle(new NotificationCompat.BigTextStyle().bigText("Big View Styles"))
+                .setContentIntent(resultPendingIntent)
+                .addAction(R.drawable.ic_launcher_foreground, "Add", resultPendingIntent)
+
+
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+
+        // Envoyer la notification
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        notificationManager.notify(1, notificationBuilder.build());
 
     }
 }
--- app/src/main/java/com/ynov/vernet/botbubulle/MyFirebaseMessagingService.java
+++ app/src/main/java/com/ynov/vernet/botbubulle/MyFirebaseMessagingService.java
@@ -2,6 +2,10 @@ package com.ynov.vernet.botbubulle;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.ContentResolver;
+import android.graphics.Color;
+import android.media.AudioAttributes;
+import android.net.Uri;
 import android.os.Build;
 
 import androidx.annotation.NonNull;
@@ -18,11 +22,27 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
 
+        Uri sound = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + this.getPackageName() + "/" + R.raw.bulles);  //Here is FILE_NAME is the name of file that you want to play
+        AudioAttributes attributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                .build();
+
+
         // Créer la notification
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.bubulle)
+                .setSmallIcon(R.drawable.icon)
                 .setContentText("Dring Dring !")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                //Vibration
+                .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+                .setStyle(new NotificationCompat.BigTextStyle())
+
+                //LED
+                .setLights(Color.RED, 3000, 3000)
+
+                //Ton
+//        builder.setSound(Uri.parse("uri://sadfasdfasdf.mp3"));
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
 
         // Envoyer la notification
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
@@ -31,6 +51,10 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             String description = getString(R.string.channel_description);
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+
+            channel.enableLights(true);
+            channel.enableVibration(true);
+            channel.setSound(sound, attributes); // This is IMPORTANT
             channel.setDescription(description);
             NotificationManager notificationManager2 = getSystemService(NotificationManager.class);
             notificationManager2.createNotificationChannel(channel);
