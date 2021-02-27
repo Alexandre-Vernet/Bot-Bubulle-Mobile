c8bc9341131f0ba1f69e2d0a41dc412e07c910e5
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2021-02-27 16:43:05 +0100

    Add files ReadMe

--- app/src/main/java/com/ynov/vernet/botbubulle/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/botbubulle/MainActivity.java
@@ -6,10 +6,13 @@ import android.app.AlarmManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.BitmapFactory;
+import android.media.AudioAttributes;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TimePicker;
@@ -82,16 +85,17 @@ public class MainActivity extends AppCompatActivity {
         PendingIntent alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
         alarmMgr.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
                 AlarmManager.INTERVAL_DAY, alarmIntent);
+
+//        sendNotification();
     }
 
     private void sendNotification() {
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
         // Prepare onclick notification redirection
         Intent repeating_intent = new Intent(context, MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 100, repeating_intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        // Button "remember in 30mn"
+        // Button "send sms"
         Intent iSMS = new Intent(context, SMS.class);
         PendingIntent pIntentSMS = PendingIntent.getBroadcast(context, 1, iSMS, PendingIntent.FLAG_UPDATE_CURRENT);
 
@@ -103,9 +107,11 @@ public class MainActivity extends AppCompatActivity {
                 .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.icon))
                 .addAction(R.drawable.ic_launcher_foreground, "Envoyer un msg", pIntentSMS)
                 .setColor(ContextCompat.getColor(context, R.color.notification))
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setAutoCancel(true);
 
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
         // Create channel
         String channelId = "id";
@@ -115,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
         notificationManager.createNotificationChannel(notificationChannel);
         builder.setChannelId(channelId);
 
-        // Send notification
+        // Display notification
         notificationManager.notify(100, builder.build());
     }
 }
