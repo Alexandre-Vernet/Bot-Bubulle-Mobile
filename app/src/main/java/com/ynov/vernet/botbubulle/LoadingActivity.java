244161357d1fda1942e60397eb122ec113896e20
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-07-04 20:53:44 +0200

    Remove Biometric & code activity

--- app/src/main/java/com/ynov/vernet/botbubulle/LoadingActivity.java
+++ app/src/main/java/com/ynov/vernet/botbubulle/LoadingActivity.java
@@ -1,8 +1,6 @@
 package com.ynov.vernet.botbubulle;
 
-import android.content.Context;
 import android.content.Intent;
-import android.hardware.fingerprint.FingerprintManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
@@ -16,33 +14,15 @@ public class LoadingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chargement);
 
-        final Context context = this;
-
         // Play bubbles sound
         final MediaPlayer mediaPlayerBubbles = MediaPlayer.create(this, R.raw.bulles);
         mediaPlayerBubbles.start();
 
         // 2s of loading
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                FingerprintManager fingerprintManager = (FingerprintManager) context.getSystemService(Context.FINGERPRINT_SERVICE);
-
-                // If phone has fingerprint sensor
-                assert fingerprintManager != null;
-                if (fingerprintManager.isHardwareDetected() || fingerprintManager.hasEnrolledFingerprints()) {
-
-                    // Log with fingerprint
-                    Intent intent = new Intent(getApplicationContext(), BiometricsActivity.class);
-                    startActivity(intent);
-                    finish();
-                } else {
-                    // Log with code
-                    Intent intent = new Intent(getApplicationContext(), CodeActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
-            }
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
         }, 2000);
     }
 }
